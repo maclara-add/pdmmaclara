f4171d61a42869d02d64ae572e88d7946063588f
ma-claraad <adaoclaraa@gmail.com>
2025-09-21 23:01:00 -0300

    botoes

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.aula2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     Button button;
-
-
+    EditText edPeso, edAltura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +21,22 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        button.setOnClickListener(View);
+        button=findViewById(R.id.button);
+        edPeso=findViewById(R.id.edPeso);
+        edAltura=findViewById(R.id.edAltura);
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, imcresultado.class);
+            Bundle bundle = new Bundle();
+
+            Double peso = Double.parseDouble(edPeso.getText().toString());
+            Double altura = Double.parseDouble(edAltura.getText().toString());
+            bundle.putDouble("peso",peso);
+            bundle.putDouble("altura",altura);
+
+            intent.putExtras(bundle);
+            startActivity(intent);
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
