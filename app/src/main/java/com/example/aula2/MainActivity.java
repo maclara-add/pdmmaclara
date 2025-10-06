b9d213bcf2bff90ad6951c276ca87e9b2b4f7209
maclara-add <adaoclaraa@gmail.com>
2025-10-06 09:06:36 -0300

    Listagem com array

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.aula2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    Button bEntrar;
+
+    EditText editText;
+    ArrayList<String> nomes ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        listView = findViewById(R.id.listView);
+        bEntrar = findViewById(R.id.bottEntrar);
+        editText = findViewById(R.id.editTextText);
+
+
+        nomes = new ArrayList<String>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nomes);
+
+        listView.setAdapter(adapter);
+        bEntrar.setOnClickListener(( view) -> {
+            nomes.add(editText.getText().toString());
+            adapter.notifyDataSetChanged();
         });
+
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+            nomes.remove(position);
+            adapter.notifyDataSetChanged();
+            return true;
+        });
+
     }
 }
