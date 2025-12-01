9c68dda75b814fde099bb280a635db0e802f158a
maclara-add <adaoclaraa@gmail.com>
2025-12-01 08:21:05 -0300

    sensores

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,68 +1,47 @@
 package com.example.aula2;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.aula2.R;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    int i=0;
+    SensorManager mSensorManager;
+    Sensor sensor;
+    TextView tx,ty,tz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tx =findViewById(R.id.textViewX);
+        ty =findViewById(R.id.textViewY);
+        tz =findViewById(R.id.textViewZ);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+//        sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("Ciclo_vida", "OnStart");
-        Log.wtf("Ciclo_vida", "OnStart");;
-    }
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        tx.setText("X:"+Float.toString(sensorEvent.values[0]));
+        ty.setText("Y:"+Float.toString(sensorEvent.values[1]));
+        tz.setText("Z:"+Float.toString(sensorEvent.values[2]));
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("Ciclo_vida", "OnRestart");
-        Log.wtf("Ciclo_vida", "OnRestart");;
     }
-
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("Ciclo_vida", "OnResume");
-        Log.wtf("Ciclo_vida", "OnResume");;
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("Ciclo_vida", "OnPause");
-        Log.wtf("Ciclo_vida", "OnPause");;
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("Ciclo_vida", "OnStop");
-        Log.wtf("Ciclo_vida", "OnStop");;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("Ciclo_vida", "OnDestroy");
-        Log.wtf("Ciclo_vida", "OnDestroy");;
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 
