851e9f86317b00f9da3e7944ed6faae3f1fb374b
maclara-add <adaoclaraa@gmail.com>
2025-12-01 07:50:34 -0300

    oi

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.aula2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,30 +13,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import com.example.aula2.R;
 
 public class MainActivity extends AppCompatActivity {
+    String[] nomes = new String[] {
+            "Maria Clara", "Naju", "Natan",
+            "Sarah", "Clarocas"
+    };
 
-    String[] nomes = new String[]{"César", "José", "Clara", "Samanta", "Camila", "Gustavo"};
-    ListView listView;
+    ListView lv; // os nomes q vao aparecer
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            listView = findViewById(R.id.listview);
-            return insets;
-        });
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                nomes);
-        listView.setAdapter(adapter);
 
+        // recupera a list view na lv
+        lv = findViewById(R.id.listview);
+
+        // conecta os nomes do layout a cada item
+        ArrayAdapter<String> a = new ArrayAdapter(
+                this,
+                R.layout.item_lista,
+                R.id.textView,
+                nomes // vetor nomes
+        );
+
+        // p exibir o nome
+        lv.setAdapter(a);
     }
 }
