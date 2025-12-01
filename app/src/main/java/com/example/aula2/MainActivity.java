2b81001dacd33f7fb7a9621bff63de1f00ba6871
maclara-add <adaoclaraa@gmail.com>
2025-12-01 08:51:33 -0300

    ooooooooooooooooooooooe

--- app/src/main/java/com/example/aula2/MainActivity.java
+++ app/src/main/java/com/example/aula2/MainActivity.java
@@ -6,47 +6,49 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.aula2.R;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    SensorManager sm;
+    TextView textView;
+    ListView listView;
 
-    //declaracao das variaveis
-    int i=0;
-    SensorManager mSensorManager;
-    Sensor sensor;
-    TextView tx,ty,tz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // variaveis
-        tx =findViewById(R.id.textViewX);
-        ty =findViewById(R.id.textViewY);
-        tz =findViewById(R.id.textViewZ);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textViewSensor);
+        listView = findViewById(R.id.listView);
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sm.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-    // obtem o sensor manager
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        // sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-        //acessando o acelerometro
-        sensor=mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        mSensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+        List<Sensor> sensorList = sm.getSensorList(Sensor.TYPE_ALL);
+        ArrayList<String> listNameSensor = new ArrayList<>();
+        for (Sensor s: sensorList) {
+            listNameSensor.add(s.getName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listNameSensor);
+        listView.setAdapter(adapter);
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        tx.setText("X:"+Float.toString(sensorEvent.values[0]));
-        ty.setText("Y:"+Float.toString(sensorEvent.values[1]));
-        tz.setText("Z:"+Float.toString(sensorEvent.values[2]));
-
+    public void onSensorChanged(SensorEvent event) {
+        event.sensor.getName();
+        textView.setText(Float.toString(event.values[0]));
     }
+
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
-
 }
